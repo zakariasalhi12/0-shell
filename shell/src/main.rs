8e6030b7000cab5107738a599fb17e45c0d3bfb2
zakariasalhi12 <rafayelle2005@gmail.com>
2025-06-04 21:42:07 +0100

    listen to stdin

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -1,3 +1,31 @@
+use std::io::{self, Write};
+
+
+enum Colors {
+    White(String),
+    Black(String),
+    Blue(String),
+    Red(String),
+}
+
+impl Colors {
+    fn to_ansi(&self) -> String {
+        match self {
+            Colors::White(text) => format!("\x1b[37m{}\x1b[0m", text),
+            Colors::Black(text) => format!("\x1b[30m{}\x1b[0m", text),
+            Colors::Blue(text) => format!("\x1b[34m{}\x1b[0m", text),
+            Colors::Red(text) => format!("\x1b[31m{}\x1b[0m", text),
+        }
+    }
+}
+
 fn main() {
-    
+    let mut buffer = String::new();
+    loop {
+        let mut Prompt = Colors::Blue(String::from("➜ :")); 
+        print!("{}" , Prompt.to_ansi());
+        io::stdout().flush().unwrap();
+        buffer.clear();
+        io::stdin().read_line(&mut buffer).unwrap();
+    }
 }
