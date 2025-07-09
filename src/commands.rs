82b6aea9c51f1eb21443987cd5be7157fb9efd53
youssefhajjaoui <youssefhajjaoui67@gmail.com>
2025-07-09 17:53:46 +0100

    fixing echo new line

--- src/commands.rs
+++ src/commands.rs
@@ -1,7 +1,18 @@
 use std::io::Write;
 pub fn echo(args: Vec<&str>) {
-    if args.len() == 0 {println!("");return}
-    args.iter().for_each(|c| {print!("{c} ");std::io::stdout().flush();});
+    if args.len() == 0 {
+        println!("");
+        return;
+    }
+    args.iter().enumerate().for_each(|(i, c)| {
+        if i != args.len() - 1{
+            print!("{c} ");
+            std::io::stdout().flush();
+        }else {
+             println!("{c}");
+        }
+        
+    });
 }
 pub fn cd() {}
 pub fn ls() {}
