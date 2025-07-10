7a14ae57ae4d83f5b3f88fe17baf311ea5030aa9
zakariasalhi12 <rafayelle2005@gmail.com>
2025-07-10 22:57:45 +0100

    impl mkdir

--- src/commands/mkdir.rs
+++ src/commands/mkdir.rs
@@ -1,5 +1,9 @@
 use crate::ShellCommand;
 
+use std::{fs, path::Path};
+use std::io::ErrorKind;
+use std::io;
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct mkdir {
     pub args: Vec<String>,
@@ -12,7 +16,15 @@ impl mkdir {
 }
 
 impl ShellCommand for mkdir {
-    fn execute(&self) -> std::io::Result<()> {
+    fn execute(&self) -> io::Result<()> {
+        for arg in &self.args {
+            let path = Path::new(arg);
+            match fs::create_dir(path) {
+                Ok(_) => continue,
+                Err(ref e) if e.kind() == ErrorKind::AlreadyExists => continue,
+                Err(e) => return Err(e),
+            }
+        }
         Ok(())
     }
 }
