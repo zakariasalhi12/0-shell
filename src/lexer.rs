40772604b7dc89f9fa84143fb2b2cca42be55070
e-aub <eaub@proton.me>
2025-07-13 19:11:50 +0100

    working on lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -20,7 +20,7 @@ pub enum Token {
 
 pub fn tokenize(input: &str) -> Result<Vec<Token>, ShellError> {
     let mut tokens : Vec<Token> = vec![];
-    let mut chars : Vec<char> = input.chars().collect();
+    let chars : Vec<char> = input.chars().collect();
     let mut inSingleQuote = false;
     let mut inDoubleQuote = false;
     let mut escaping = false;
@@ -44,8 +44,108 @@ pub fn tokenize(input: &str) -> Result<Vec<Token>, ShellError> {
             i+=1;
             continue;
         }
+
+        if c == '\'' && !inDoubleQuote{
+            inSingleQuote = !inSingleQuote;
+            i+=1;
+            continue;
+        }
+
+        if c == '"' && !inSingleQuote{
+            inDoubleQuote = !inDoubleQuote;
+            i+=1;
+            continue;
+        }
+
+        if inSingleQuote || inDoubleQuote{
+            token_buf.push(c);
+            i+=1;
+            continue;
+        }
+
+
+        if c.is_whitespace(){
+            if !token_buf.is_empty(){
+                tokens.push(Token::Word(token_buf.clone()));
+                token_buf.clear();
+            }
+
+            if c == '\n'{
+                tokens.push(Token::Newline);
+            }
+
+            i+=1;
+            continue;
+        }
+
+        if let Some(three) = chars.get(i ..= i+2){
+            let three : String = three.iter().collect();
+
+            if three == "<<<"{
+                flush_buf(&mut token_buf, &mut tokens);
+                tokens.push(Token::HereString);
+                i+=3;
+                continue;
+            }
+        }
+
+        if let Some(two) = chars.get(i ..= i+1){
+            let two : String = two.iter().collect();
+
+            match two.as_str(){
+                "&&" =>{
+
+                },
+                "||" => {
+
+                },
+                ">>" => {
+
+                },
+                "<<" => {
+
+                },
+                ">&" => {
+
+                },
+                "<&" => {
+
+                },
+                ";;" => {
+
+                },
+                              
+                _ =>{
+
+                }
+            }
+        }
+
+       match c {
+    '|' => {},
+    '&' => {},
+    ';' => {},
+    '<' => {},
+    '>' => {},
+    '!' => {},
+    '(' => {},
+    ')' => {},
+    '{' => {},
+    '}' => {},
+    '$' => {},
+    _ => {},
+}
+
     }
         
 
     Ok(vec![])
+}
+
+
+fn flush_buf(buf: &mut String, tokens: &mut Vec<Token>) {
+    if !buf.is_empty() {
+        tokens.push(Token::Word(buf.clone()));
+        buf.clear();
+    }
 }
