af935c5def9e631c20ac1f3cea523f16449b8999
e-aub <eaub@proton.me>
2025-07-15 04:16:20 +0100

    working on command substitution and arithmitic substitution

--- src/expansion.rs
+++ src/expansion.rs
@@ -1,5 +1,45 @@
 use crate::env::ShellEnv;
 
+#[derive(Debug, Clone)]
+pub enum ArithmeticToken {
+    Number(i64),
+    Variable(String),
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Modulo,
+    Assign,
+    AddAssign,
+    SubAssign,
+    MulAssign,
+    DivAssign,
+    ModAssign,
+    Increment,
+    Decrement,
+    Equal,
+    NotEqual,
+    Less,
+    Greater,
+    LessEqual,
+    GreaterEqual,
+    LogicalAnd,
+    LogicalOr,
+    LogicalNot,
+    BitAnd,
+    BitOr,
+    BitXor,
+    BitNot,
+    ShiftLeft,
+    ShiftRight,
+    LParen,
+    RParen,
+    QuestionMark,
+    Colon,
+    Substitution(Vec<ArithmeticToken>),
+}
+
+
 
 pub fn expand(input: &str, env: &ShellEnv) -> String {
     // handle $VAR, ${VAR:-default}, $((1+2)), etc.
