693ba5a9a284de7f6b6057c4a23ec1832fb3632d
youssefhajjaoui <youssefhajjaoui67@gmail.com>
2025-07-21 19:16:40 +0100

    fixing error if stdout is not  a tty

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use core::error;
+
 use shell::events_handler::*;
 use termion::*;
 
 fn main() {
-    let mut shell = Shell::new();
+    let mut shell = match Shell::new() {
+        Ok(val) => val,
+        Err(e) => return,
+    };
+
     shell.run();
 }
