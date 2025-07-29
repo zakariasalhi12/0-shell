f1394a7739efb324a7db1b12f7c076c9c20d1e40
youssefhajjaoui <youssefhajjaoui67@gmail.com>
2025-07-29 15:03:29 +0100

    clean main

--- src/main.rs
+++ src/main.rs
@@ -15,46 +15,4 @@ fn main() {
         Err(_) => return,
     };
     shell.run();
-    // let mut buffer = String::new();
-    let mut env = ShellEnv::new();
-    // {
-    //     let map = ENV.lock().unwrap();
-
-    //     for (k, v) in map.iter() {
-    //         println!("{} : {}", k, v);
-    //     }
-    // }
 }
-
-// pub fn Parse_input(buffer: &str) {
-//     match lexer::tokenize::Tokenizer::new(buffer.trim().to_owned().as_str()).tokenize() {
-//         Ok(res) => {
-//             // println!("{}", "== Tokens ==".bold().bright_blue());
-//             // for token in &res {
-//             //     println!("{:#?}", token);
-//             // }
-//             match Parser::new(res).parse() {
-//                 Ok(ast) => {
-//                     println!("{}", "== AST Output ==".bold().bright_yellow());
-//                     match ast {
-//                         Some(ast) => {
-//                             println!("{}", ast); // Optionally keep for debugging
-//                             let exit_code = execute(&ast, &mut envirement).unwrap_or(1);
-//                             println!("[exit code: {}]", exit_code);
-//                         }
-//                         None => println!("empty AST"),
-//                     }
-//                 }
-//                 Err(e) => {
-//                     eprintln!("{}", "== AST Parse Error ==".bold().red());
-//                     eprintln!("{:#?}", e);
-//                 }
-//             }
-//         }
-
-//         Err(err) => {
-//             eprintln!("{}", "== Tokenization Error ==".bold().red());
-//             eprintln!("{:#?}", err);
-//         }
-//     }
-// }
