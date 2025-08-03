25960e59f556247e8488eaefe305ff15e806c23a
youssefhajjaoui <youssefhajjaoui67@gmail.com>
2025-08-03 02:48:46 +0100

    redirection

--- cat/target/release/libcat.d
+++
@@ -1,1 +0,0 @@
-/home/yhajjaou/0-shell/cat/target/release/libcat.rlib: /home/yhajjaou/0-shell/cat/src/lib.rs
--- ls/target/release/ls.d
+++
@@ -1,1 +0,0 @@
-/home/yhajjaou/0-shell/ls/target/release/ls: /home/yhajjaou/0-shell/ls/src/lib.rs /home/yhajjaou/0-shell/ls/src/main.rs
