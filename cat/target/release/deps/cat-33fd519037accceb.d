0e65feca2ba6df12be89e0e56a4f35c2408dded4
youssefhajjaoui <youssefhajjaoui67@gmail.com>
2025-08-06 00:09:36 +0100

    handling qotes

--- cat/target/release/deps/cat-33fd519037accceb.d
+++
@@ -1,5 +0,0 @@
-/home/yhajjaou/Desktop/0-shell/cat/target/release/deps/cat-33fd519037accceb.d: src/main.rs
-
-/home/yhajjaou/Desktop/0-shell/cat/target/release/deps/cat-33fd519037accceb: src/main.rs
-
-src/main.rs:
--- ls/target/release/ls.d
+++
@@ -1,1 +0,0 @@
-/home/yhajjaou/Desktop/0-shell/ls/target/release/ls: /home/yhajjaou/Desktop/0-shell/ls/src/lib.rs /home/yhajjaou/Desktop/0-shell/ls/src/main.rs
