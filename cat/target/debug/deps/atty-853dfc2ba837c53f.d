329c283aba0bbebe2c426ed6deb93ed90ea64316
zakariasalhi12 <rafayelle2005@gmail.com>
2025-08-20 03:23:42 +0100

    fix: Counting characters in buffer instead of bytes

--- cat/target/debug/deps/atty-853dfc2ba837c53f.d
+++
@@ -1,5 +0,0 @@
-/home/zsalhi/0-shell/cat/target/debug/deps/atty-853dfc2ba837c53f.d: /home/zsalhi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/atty-0.2.14/src/lib.rs
-
-/home/zsalhi/0-shell/cat/target/debug/deps/libatty-853dfc2ba837c53f.rmeta: /home/zsalhi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/atty-0.2.14/src/lib.rs
-
-/home/zsalhi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/atty-0.2.14/src/lib.rs:
--- cat/target/release/deps/cat-48bf55d6b63c3f9f.d
+++
@@ -1,5 +0,0 @@
-/home/zsalhi/0-shell/cat/target/release/deps/cat-48bf55d6b63c3f9f.d: src/main.rs
-
-/home/zsalhi/0-shell/cat/target/release/deps/cat-48bf55d6b63c3f9f: src/main.rs
-
-src/main.rs:
