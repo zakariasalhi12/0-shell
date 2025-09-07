9dfd5e7882620c8c10462517777a0c372935a5fa
youssefhajjaoui <youssefhajjaoui67@gmail.com>
2025-09-07 18:27:55 +0100

    need logic to signals listening in fg and bg

--- shell/src/commands/fg.rs
+++ shell/src/commands/fg.rs
@@ -1,6 +1,6 @@
-use nix::sys::signal::{signal, Signal};
-
+use crate::features::jobs::JobStatus;
 use crate::{ShellCommand, envirement::ShellEnv};
+use nix::sys::signal::{Signal, signal};
 
 pub struct Fg {
     args: Vec<String>,
@@ -8,9 +8,7 @@ pub struct Fg {
 
 impl Fg {
     pub fn new(args: Vec<String>) -> Self {
-        Fg {
-            args,
-        }
+        Fg { args }
     }
 }
 
@@ -28,14 +26,16 @@ impl ShellCommand for Fg {
         let pgid = job.pgid;
 
         // Ignore SIGTTOU so shell doesn't get suspended
-        let old = unsafe { signal(Signal::SIGTTOU, nix::sys::signal::SigHandler::SigIgn) }
-            .unwrap();
+        let old = unsafe { signal(Signal::SIGTTOU, nix::sys::signal::SigHandler::SigIgn) }.unwrap();
 
         // Move job to foreground
         if let Err(e) = nix::unistd::tcsetpgrp(nix::libc::STDIN_FILENO, pgid) {
             eprintln!("fg: failed to set terminal control: {}", e);
             unsafe { signal(Signal::SIGTTOU, old).unwrap() };
-            return Err(std::io::Error::new(std::io::ErrorKind::Other, "tcsetpgrp failed"));
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "tcsetpgrp failed",
+            ));
         }
 
         // Restore SIGTTOU
@@ -47,18 +47,28 @@ impl ShellCommand for Fg {
         // Wait for the job to finish or stop again
         loop {
             match nix::sys::wait::waitpid(pgid, Some(nix::sys::wait::WaitPidFlag::WUNTRACED)) {
-                Ok(nix::sys::wait::WaitStatus::Exited(_, _)) |
-                Ok(nix::sys::wait::WaitStatus::Signaled(_, _, _)) => break,
-                Ok(nix::sys::wait::WaitStatus::Stopped(_, _)) => break,
+                Ok(wait_status) => match wait_status {
+                    nix::sys::wait::WaitStatus::Exited(_, code) => {
+                        // Remove job when process exits
+                        env.jobs.remove_job(pgid);
+                    }
+                    nix::sys::wait::WaitStatus::Signaled(_, _, _) => {
+                        env.jobs.remove_job(pgid);
+                    }
+                    nix::sys::wait::WaitStatus::Stopped(_, _) => {
+                        println!("[{}]+ Stopped", pgid);
+                        env.jobs.update_job_status(pgid, JobStatus::Stopped);
+                    }
+                    _ => break,
+                },
                 Err(_) => break,
-                _ => (),
+                // No need for a wildcard arm anymore
             }
         }
 
         // Return terminal control to shell
         let shell_pgid = nix::unistd::getpgrp();
-        let old = unsafe { signal(Signal::SIGTTOU, nix::sys::signal::SigHandler::SigIgn) }
-            .unwrap();
+        let old = unsafe { signal(Signal::SIGTTOU, nix::sys::signal::SigHandler::SigIgn) }.unwrap();
         nix::unistd::tcsetpgrp(nix::libc::STDIN_FILENO, shell_pgid).ok();
         unsafe { signal(Signal::SIGTTOU, old).unwrap() };
 
